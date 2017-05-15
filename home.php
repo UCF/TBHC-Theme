8b03e59876c93c00135d29e70a8a67bb65f246cf
LemondRinds <diveaway89@gmail.com>
2017-05-15 02:39:02 -0400

    debug alt home page theme

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <?php $options = get_option(THEME_OPTIONS_NAME);?>
 <?php $page    = get_page_by_title('Home');?>
 	<div class="row" id="home" data-template="home-nodescription" role="main">
-		<?=print_r(get_theme_option('home_page_theme');frontpage_opportunities();?>
+		<?=print_r(get_option('home_page_theme');frontpage_opportunities();?>
 		<?=frontpage_interests()?>
  		<?=frontpage_spotlights()?>
 		<?=frontpage_events()?>
