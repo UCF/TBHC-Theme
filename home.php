c619cca9775a82e4e192a9001f8ee53bf83b7233
LemondRinds <diveaway89@gmail.com>
2017-06-06 10:28:59 -0400

    home.php edit

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 	<div class="row" id="home" data-template="home-nodescription" role="main">
 		<?=get_theme_option('home_page_theme') != '0' ? '' : frontpage_opportunities() ?>
 		<?=frontpage_interests()?>
- 		<?=get_theme_option('home_page_theme') != '0' ? '' : frontpage_spotlights()?>
+ 		<?=get_theme_option('home_page_theme') == '1' ? '' : frontpage_spotlights()?>
 		<?=get_theme_option('home_page_theme') == '2' ? '' : frontpage_events()?>
 	</div>
 	<!--<div class="container-shadow">
