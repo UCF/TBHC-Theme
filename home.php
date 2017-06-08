e9669f3cfb9cdcca545c9037f956afe33d36a3ed
LemondRinds <diveaway89@gmail.com>
2017-06-07 22:46:25 -0400

    weird bug

--- home.php
+++ home.php
@@ -4,8 +4,8 @@
 	<div class="row" id="home" data-template="home-nodescription" role="main">
 		<?=get_theme_option('home_page_theme') === '0' ? frontpage_opportunities() : ''?>
 		<?=get_theme_option('home_page_theme') == '2' ? '' : frontpage_interests()?>
-		<?if(DEBUG){print_r(get_theme_option('home_page_theme'));print_r(gettype(get_theme_option('home_page_theme')));print_r(get_theme_option('home_page_theme')==='0');}?>
- 		<?=get_theme_option('home_page_theme') == '2' ? frontpage_scholarship_spotlight() : get_theme_option('home_page_theme') === '0' ? frontpage_spotlights() : ''?>
+		<?if(DEBUG){print_r(get_theme_option('home_page_theme'));print_r(gettype(get_theme_option('home_page_theme')));print_r(get_theme_option('home_page_theme')=='2');}?>
+ 		<?=get_theme_option('home_page_theme') == '2' ? frontpage_scholarship_spotlight() : ''?><?=get_theme_option('home_page_theme') === '0' ? frontpage_spotlights() : ''?>
 		<?=get_theme_option('home_page_theme') == '2' ? '' : frontpage_events()?>
 	</div>
 	<!--<div class="container-shadow">
