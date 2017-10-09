ab2d570150fc3e8f12938eab785c13860e3be249
LemondRinds <diveaway89@gmail.com>
2017-10-09 16:50:50 -0400

    mistyped hook callback name

--- functions.php
+++ functions.php
@@ -2596,8 +2596,8 @@ function add_tinymce_font($init) {
     }
     $font_formats = isset($init['font_formats']) ? $init['font_formats'] : 'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats';
     $custom_fonts = ';'.'SeaSide=seaside;Journal=journal';
-    $init['font_formats'] = $font_formats . $custom_fonts;
-    return $init;*/
+    $init['font_formats'] = $font_formats . $custom_fonts;*/
+    return $init;
 }
 add_filter('tiny_mce_before_init', 'add_tinymce_font');
 
